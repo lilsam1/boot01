f0c9ccadb9904354b51c3b56559efe4f90ffefb1
lilsam1 <ghkseo3@gmail.com>
2023-03-15 12:29:34 +0900

    230315

---
+++ src/main/java/com/example/boot01/config/RootConfig.java
@@ -0,0 +1,18 @@
+package com.example.boot01.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+
+public class RootConfig {
+    @Bean
+    public ModelMapper getMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return  modelMapper;
+    }
+}
